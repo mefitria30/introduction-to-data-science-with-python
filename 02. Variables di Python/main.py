61db3c34d43d05d72832e5e17ff5d2553dc92b7f
Fitria Melliyanni <fitria.melliyanni@dataon.com>
2025-02-02 17:15:13 +0700

    Variables di Python

---
+++ 02. Variables di Python/main.py
@@ -0,0 +1,19 @@
+# Berikut adalah tiga aturan penting yang wajib diikuti pada saat kita mendeklarasikan suatu variabel di Python. 
+
+# Nama dari sebuah variabel harus dimulai dengan huruf (a-z, A-Z) atau karakter garis bawah/underscore (_) dan tidak dapat dimulai dengan angka (0-9).
+
+# Variabel hanya boleh mengandung karakter alfabet dan bilangan dan underscore (a-z, A-Z, 0-9, _)
+ 
+# Variabel bersifat case-sensitive yang mengartikan bahwa variabel TINGGI, tinggi, dan Tinggi merujuk pada tiga variabel berbeda.
+
+bil1 = 10
+Bil_2 = 20
+Frasa = "Halo Dunia"
+bil1, Bil_2 = 10, 20
+salam = "Selamat Pagi"; Penutup = "Salam Sejahtera"
+
+print(bil1)
+print(Bil_2)
+print(Frasa)
+print(salam)
+print(Penutup)
