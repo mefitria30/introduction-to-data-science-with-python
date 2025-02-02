02903378f91df17086579303ff3dfa35e2c77b70
Fitria Melliyanni <fitria.melliyanni@dataon.com>
2025-02-02 17:23:22 +0700

    Comments di Python

---
+++ 03. Comments di Python/main.py
@@ -0,0 +1,9 @@
+# Comments adalah sekumpulan teks yang dituliskan dalam sebuah program dan tidak akan mempengaruhi hasil dari sebuah program.
+
+# peritah pada baris ini tidak mempengaruhi program
+'''
+perintah ini tidak akan dieksekusi oleh Python
+dan perintah ini juga tidak akan dieksekusi
+perintah ini juga tidak akan dieksekusi
+'''
+print("jadi # digunakan untuk membuat comment pada Python")
